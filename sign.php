e490020e33b65366b0fd879d34f5f1a53f58483b
jgzuke <jgzuke@gmail.com>
2014-11-29 23:27:59 -0500

    Hey it all works and compiles

---
+++ sign.php
@@ -0,0 +1,43 @@
+<?php
+header('Location: '."/guestbook");
+
+use google\appengine\api\users\User;
+use google\appengine\api\users\UserService;
+
+$user = UserService::getCurrentUser();
+
+$db = null;
+if (isset($_SERVER['SERVER_SOFTWARE']) &&
+strpos($_SERVER['SERVER_SOFTWARE'],'Google App Engine') !== false) {
+  // Connect from App Engine.
+  try{
+     $db = new pdo('mysql:unix_socket=/cloudsql/<your-project-id>:<your-instance-name>;dbname=guestbook', 'root', '');
+  }catch(PDOException $ex){
+      die(json_encode(
+          array('outcome' => false, 'message' => 'Unable to connect.')
+          )
+      );
+  }
+} else {
+  // Connect from a development environment.
+  try{
+     $db = new pdo('mysql:host=127.0.0.1:8889;dbname=guestbook', 'root', 'temppass');
+  }catch(PDOException $ex){
+      die(json_encode(
+          array('outcome' => false, 'message' => 'Unable to connect')
+          )
+      );
+  }
+}
+try {
+  if (array_key_exists('content', $_POST)) {
+    $stmt = $db->prepare('INSERT INTO entries (guestName, content) VALUES (:name, :content)');
+    $stmt->execute(array(':name' => htmlspecialchars($user->getNickname()), ':content' => htmlspecialchars($_POST['content'])));
+    $affected_rows = $stmt->rowCount();
+    // Log $affected_rows.
+  }
+} catch (PDOException $ex) {
+  // Log error.
+}
+$db = null;
+?>
