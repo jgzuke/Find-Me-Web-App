456cc32941b6fff8cf904b5b3ed0869f7b3b9095
jgzuke <jgzuke@gmail.com>
2014-11-30 23:51:00 -0500

    New data

--- guestbook.php
+++ guestbook.php
@@ -2,64 +2,88 @@
   <link type="text/css" rel="stylesheet" href="/css/main.css" />
 </head>
 <html>
- <body>
+
+<body>
   <?php
-  use google\appengine\api\users\User;
-  use google\appengine\api\users\UserService;
+    use google\appengine\api\users\User;
+    use google\appengine\api\users\UserService;
 
-  $user = UserService::getCurrentUser();
+    $user = UserService::getCurrentUser();
 
-  if ($user) {
-    echo 'Hello, ' . htmlspecialchars($user->getNickname());
-  }
-  else {
-    header('Location: ' . UserService::createLoginURL($_SERVER['REQUEST_URI']));
-  }
-  ?>
-
-  <h2>Guestbook Entries</h2>
-  <?php
-  // Create a connection.
-
-  $db = null;
-  if (isset($_SERVER['SERVER_SOFTWARE']) &&
-  strpos($_SERVER['SERVER_SOFTWARE'],'Google App Engine') !== false) {
-    // Connect from App Engine.
-    try{
-       $db = new pdo('mysql:unix_socket=/cloudsql/findmewebapp:cloudinstanceid;dbname=guestbook', 'root', 'temppass');
-    }catch(PDOException $ex){
-        die(json_encode(
-            array('outcome' => false, 'message' => 'Unable to connect.')
-            )
-        );
+    if ($user) {
+      echo 'Hello, ' . htmlspecialchars($user->getNickname());
     }
-  } else {
-    // Connect from a development environment.
-    try{
-       $db = new pdo('mysql:host=127.0.0.1:8889;dbname=guestbook', 'root', 'temppass');
-    }catch(PDOException $ex){
-        die(json_encode(
-            array('outcome' => false, 'message' => 'Unable to connect')
-            )
-        );
+    else {
+      header('Location: ' . UserService::createLoginURL($_SERVER['REQUEST_URI']));
     }
-  }
-  try {
-    // Show existing guestbook entries.
-    foreach($db->query('SELECT * from entries') as $row) {
-            echo "<div><strong>" . $row['guestName'] . "</strong> wrote <br> " . $row['content'] . "</div>";
-     }
-  } catch (PDOException $ex) {
-    echo "An error occurred in reading or writing to guestbook.";
-  }
-  $db = null;
   ?>
-
   <h1 id="topname">Find Me</h1>
+  <hr width="100%" background-color="#FFFFFF" size="4" height="2px"></hr>
+  <div class="row">
+    <div id="Find" class="col-md-6">
+      <h2>Find</h2>
+        <?php
+          $db = null;
+          if (isset($_SERVER['SERVER_SOFTWARE']) &&
+            strpos($_SERVER['SERVER_SOFTWARE'], 'Google App Engine') !== false)
+          {
+            // Connect from App Engine.
+            try
+            {
+              $db = new pdo('mysql:unix_socket=/cloudsql/findmewebapp:cloudinstanceid;dbname=guestbook', 'root', 'temppass');
+            }
+            catch (PDOException $ex)
+            {
+              die(json_encode(
+                array('outcome' => false, 'message' => 'Unable to connect.')
+              ));
+            }
+          }
+          else
+          {
+            // Connect from a development environment.
+            try
+            {
+              $db = new pdo('mysql:host=127.0.0.1:8889;dbname=guestbook', 'root', 'temppass');
+            }
+            catch (PDOException $ex)
+            {
+              die(json_encode(
+                array('outcome' => false, 'message' => 'Unable to connect')
+              ));
+            }
+          }
+          try
+          {
+            // Show existing guestbook entries.
+            foreach($db - > query('SELECT * from entries') as $row)
+            {
+              echo "<div><strong>".$row['name'].
+              "</strong> wrote <br> ".$row['location'].
+              "</div>";
+            }
+          }
+          catch (PDOException $ex)
+          {
+            echo "An error occurred in reading or writing to guestbook.";
+          }
+          $db = null;
+        ?>
+      <div><strong>" . $row['name'] . "</strong> wrote
+        <br> " . $row['location'] . "</div>"; } } catch (PDOException $ex) { echo "An error occurred in reading or writing to guestbook."; } $db = null; ?>
+    </div>
+    <div id="Move" class="col-md-6">
+      <h2>Move</h2>
+      <form action="/sign" method="post">
+        <div>
+          <textarea name="content" rows="3" cols="60"></textarea>
+        </div>
+        <div>
+          <input type="submit" value="Sign Guestbook">
+        </div>
+      </form>
+    </div>
+  </div>
+</body>
 
-  <form action="/sign" method="post">
-    <div><textarea name="content" rows="3" cols="60"></textarea></div>
-    <div><input type="submit" value="Sign Guestbook"></div>
-  </form>
-  </body>
 </html>
--- sign.php
+++ sign.php
@@ -31,8 +31,8 @@ strpos($_SERVER['SERVER_SOFTWARE'],'Google App Engine') !== false) {
 }
 try {
   if (array_key_exists('content', $_POST)) {
-    $stmt = $db->prepare('INSERT INTO entries (guestName, content) VALUES (:name, :content)');
-    $stmt->execute(array(':name' => htmlspecialchars($user->getNickname()), ':content' => htmlspecialchars($_POST['content'])));
+    $stmt = $db->prepare('INSERT INTO entries (itemName, itemLocation) VALUES (:name, :location)');
+    $stmt->execute(array(':name' => htmlspecialchars($_POST['content']), ':location' => htmlspecialchars($_POST['content'])));
     $affected_rows = $stmt->rowCount();
     // Log $affected_rows.
   }
