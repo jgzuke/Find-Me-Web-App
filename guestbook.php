9719ab2ffe79fb4aad0a5778c45ec8824bfd68b5
jgzuke <jgzuke@gmail.com>
2014-12-01 05:23:34 -0500

    Next replace old move entries, show all button

--- guestbook.php
+++ guestbook.php
@@ -25,7 +25,7 @@
       <div id = "Find" class="col-md-6">
         <h2>Find Item</h2>
         <div id = "searchForm">
-          <form name = "input" action="/search" method="post">
+          <form action="#" method="post">
             <input type="text" name="query"> 
             <input type="submit" value="Find" name = "search">
           </form>
@@ -33,7 +33,21 @@
         <?php
           if(isset($_POST['search']))
           {
-            // Form 1
+            $name=$_POST['query']; 
+             //connect  to the database 
+             $db=mysql_connect  ("127.0.0.1:8889", "root",  "temppass") or die ('I cannot connect to the database  because: ' . mysql_error()); 
+             //-select  the database to use 
+             $mydb=mysql_select_db("guestbook"); 
+              //-query  the database table 
+              $sql="SELECT  myItemName, myItemLocation FROM entries WHERE myItemName LIKE '%" . $name .  "%'"; 
+              //-run  the query against the mysql query function 
+              $result=mysql_query($sql); 
+              //-create  while loop and loop through result set 
+              while($row=mysql_fetch_array($result)){ 
+                      $item  =$row['myItemName']; 
+                      $location=$row['myItemLocation']; 
+                      echo "<div><strong>".$item."</strong>: ".$location."</div>";
+              } 
           } else
           {
             $db = null;
