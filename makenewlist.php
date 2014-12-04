36c39cbda25f5c4223c8897b5a0c59b648ade8b7
jgzuke <jgzuke@gmail.com>
2014-12-03 21:42:23 -0500

    people can share lists with each other

--- makenewlist.php
+++ makenewlist.php
@@ -3,9 +3,6 @@ header('Location: '."/");
 use google\appengine\api\users\User;
 use google\appengine\api\users\UserService;
 $user = UserService::getCurrentUser();
-$tempName = $user->getEmail();
-$myTableName = preg_replace('/[^A-Za-z0-9\-]/', '', $tempName);
-$currentTable = $myTableName.'default';
 $db = null;
 if (isset($_SERVER['SERVER_SOFTWARE']) &&
 strpos($_SERVER['SERVER_SOFTWARE'],'Google App Engine') !== false) {
@@ -28,6 +25,39 @@ strpos($_SERVER['SERVER_SOFTWARE'],'Google App Engine') !== false) {
       );
   }
 }
+$userName = $user->getEmail();
+$myUserName = preg_replace('/[^A-Za-z0-9\-]/', '', $userName);
+$firstUserName = preg_replace('/[^A-Za-z0-9\-]/', '', $_POST['email1']);
+$secondUserName = preg_replace('/[^A-Za-z0-9\-]/', '', $_POST['email2']);
+$thirdUserName = preg_replace('/[^A-Za-z0-9\-]/', '', $_POST['email3']);
+$dataTableName = "'".$myUserName.$_POST['name']."'";
+$dataTableShort = "'".$_POST['name']."'";
+
+
+$newTableName = $userName.$_POST['name'];
+$sql="CREATE table $dataTableName(myItemName VARCHAR(30) NOT NULL, myItemLocation VARCHAR(30) NOT NULL);";
+$db->exec($sql);
+$sql = "INSERT INTO $myUserName (itemTableName, itemTableShort) VALUES (:name, :short)";
+$stmt = $db->prepare($sql);
+$stmt->execute(array(':name' => $dataTableName, ':short' => $dataTableShort));
+$affected_rows = $stmt->rowCount();
+
+$sql = "INSERT INTO $firstUserName (itemTableName, itemTableShort) VALUES (:name, :short)";
+$stmt = $db->prepare($sql);
+$stmt->execute(array(':name' => $dataTableName, ':short' => $dataTableShort));
+$affected_rows = $stmt->rowCount();
+
+$sql = "INSERT INTO $secondUserName (itemTableName, itemTableShort) VALUES (:name, :short)";
+$stmt = $db->prepare($sql);
+$stmt->execute(array(':name' => $dataTableName, ':short' => $dataTableShort));
+$affected_rows = $stmt->rowCount();
+
+$sql = "INSERT INTO $thirdUserName (itemTableName, itemTableShort) VALUES (:name, :short)";
+$stmt = $db->prepare($sql);
+$stmt->execute(array(':name' => $dataTableName, ':short' => $dataTableShort));
+$affected_rows = $stmt->rowCount();
+
+
 try {
   if (array_key_exists('name', $_POST))
   {
